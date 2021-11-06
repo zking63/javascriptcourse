c97b20f7edbaa3cb2c36ca2f8ba7bd5213790d14
Zoe <zoe@DGA-6571.local>
2021-11-06 13:20:55 -0400

    clean up

--- Problem solving patterns/SlidingWindow.js
+++ Problem solving patterns/SlidingWindow.js
@@ -14,19 +14,13 @@ function maxSubArraySum(arr, num) {
     }
     //set the first max
     for (let i = 0; i < num; i++){
-        console.log(arr[i]);
         maxNum += arr[i];
     }
-    console.log("maxNum" + maxNum);
     tempNum = maxNum;
     for (let i = num; i < arr.length; i++){
-        console.log(arr[i - num]);
-        console.log(arr[i]);
         tempNum = tempNum - arr[i - num] + arr[i];
-        console.log(tempNum);
         maxNum = Math.max(maxNum, tempNum);
     }
-    console.log("maxNum " + maxNum);
     return maxNum;
 }
-console.log(maxSubArraySum([1,2,-100,3,5,6], -3));
+maxSubArraySum([1,2,100,3,5,6], 3);
