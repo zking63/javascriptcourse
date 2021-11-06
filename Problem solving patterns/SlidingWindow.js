6cf0e9b9f36946b373a46aca34b13f63e8339e28
Zoe <zoe@DGA-6571.local>
2021-11-06 13:06:03 -0400

    program working

--- Problem solving patterns/SlidingWindow.js
+++ Problem solving patterns/SlidingWindow.js
@@ -1,13 +1,29 @@
+//write a function that accepts a number called n and an array of numbers. 
+//It must find the max sum of n digits that are next to one and other 
+//in the array.
 function maxSubArraySum(arr, num) {
     console.log(arr);
     console.log(num);
     let maxNum = 0;
     let tempNum = 0;
+    //if the array is shorter than the number, then it's null
     if (arr.length < num){
         return null;
     }
-    for (let i =0; i < num; i++){
+    //set the first max
+    for (let i = 0; i < num; i++){
         console.log(arr[i]);
+        maxNum += arr[i];
     }
+    console.log("maxNum" + maxNum);
+    tempNum = maxNum;
+    for (let i = num; i < arr.length; i++){
+        console.log(arr[i - num]);
+        console.log(arr[i]);
+        tempNum = tempNum - arr[i - num] + arr[i];
+        console.log(tempNum);
+        maxNum = Math.max(maxNum, tempNum);
+    }
+    console.log("maxNum " + maxNum);
 }
-maxSubArraySum([1,2,3], 3);
+maxSubArraySum([1,2,100,3,5,6], 3);
